b448547cfd4f3a1e78887b879ff01fa92ac398c8
Callum Mackenzie <alexxander1611@gmail.com>
2022-04-29 21:50:09 -0600

    Add radio test

--- arduino_utils/src/lib.rs
+++ arduino_utils/src/lib.rs
@@ -1,5 +1,10 @@
 #![no_std]
 
+// use arduino_hal::spi::ChipSelectPin;
+// use avr_hal_generic::port::PinOps;
+// use core::convert::Infallible;
+// use embedded_hal::digital::v2::OutputPin;
+
 #[macro_export]
 macro_rules! upanic {
 	($($arg:tt)*) => {
@@ -18,3 +23,18 @@ macro_rules! uprint {
 		ufmt::uwriteln!($($arg)*).unwrap();
 	};
 }
+
+// struct WrappedChipSelectPin<CSPIN>(ChipSelectPin<CSPIN>);
+
+// impl<CSPIN: PinOps> OutputPin for WrappedChipSelectPin<CSPIN> {
+//     type Error = Infallible;
+
+//     fn set_low(&mut self) -> Result<(), Self::Error> {
+//         self.0.set_low();
+//         Ok(())
+//     }
+//     fn set_high(&mut self) -> Result<(), Self::Error> {
+//         self.0.set_high();
+//         Ok(())
+//     }
+// }
--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,19 @@
 
 pub mod controller;
 
-use arduino_hal::I2c;
-use arduino_millis_driver::*;
 use arduino_utils::*;
-use controller::model::*;
-use elinalgebra::*;
 use panic_halt as _;
 
 #[arduino_hal::entry]
 fn main() -> ! {
+    mpu_test();
+}
+
+fn mpu_test() -> ! {
+    use arduino_hal::I2c;
+    use arduino_millis_driver::*;
+    use controller::model::*;
+
     // Retrieve resources
     let dp = arduino_hal::Peripherals::take().unwrap();
     let pins = arduino_hal::pins!(dp);
@@ -29,18 +33,14 @@ fn main() -> ! {
     ));
     pos_input.init(&mut delay).unwrap();
 
-    pos_input
-        .calibrate()
-        .unwrap_or_else(|_| upanic!(&mut serial, "Mpu calibration failed"));
+    pos_input.calibrate().unwrap();
 
     let mut pos_data = PositionData::new();
 
     uprint!(&mut serial, "Finished setup");
     // Begin program loop
     loop {
-        pos_input
-            .read(&mut pos_data)
-            .unwrap_or_else(|_| upanic!(&mut serial, "Positional read failed"));
+        pos_input.read(&mut pos_data).unwrap();
 
         uprint!(
             &mut serial,
@@ -50,4 +50,51 @@ fn main() -> ! {
             pos_data.gyro_angle.z as i32,
         );
     }
+}
+
+fn radio_test() -> ! {
+    use arduino_hal::spi;
+    use nrf24_rs::{
+        config::{NrfConfig, PALevel},
+        Nrf24l01, SPI_MODE,
+    };
+
+    // Retrieve resources
+    let dp = arduino_hal::Peripherals::take().unwrap();
+    let pins = arduino_hal::pins!(dp);
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
+    let mut delay = arduino_hal::Delay::new();
+
+	let ce = pins.d7.into_output(); // Chip enable
+    let ss = pins.d10.into_output(); // Exchange ss for cs
+    let sck = pins.d13.into_output(); // Serial clock
+    let mosi = pins.d11.into_output();
+    let miso = pins.d12.into_pull_up_input();
+
+    let settings = spi::Settings {
+        data_order: spi::DataOrder::MostSignificantFirst,
+        clock: spi::SerialClockRate::OscfOver4,
+        mode: SPI_MODE,
+    };
+    let (spi, ncs) = spi::Spi::new(dp.SPI, sck, mosi, miso, ss, settings);
+	// let wcs = WrappedChipSelectPin(ncs);
+
+    let message = b"Test!";
+
+    let config = NrfConfig::default()
+        .channel(8)
+        .pa_level(PALevel::Min)
+        .payload_size(message.len() as u8);
+
+    let mut nrf_chip = Nrf24l01::new(spi, ce, ncs, &mut delay, config).unwrap();
+    if !nrf_chip.is_connected().unwrap() {
+        upanic!(&mut serial, "Could not connect");
+    }
+    nrf_chip.open_writing_pipe(b"drone").unwrap();
+
+    while let Err(_) = nrf_chip.write(&mut delay, message) {
+        arduino_hal::delay_ms(50);
+    }
+
+    loop {}
 }
